37587161abbd4e2128481944e59ae37b5463ecca
xiongyejun <244114746@qq.com>
2016-07-02 21:13:03 +0800

    strcat

--- 03-The_C_Programming_Language/05-03-strcat.c
+++ 03-The_C_Programming_Language/05-03-strcat.c
@@ -1,5 +1,13 @@
 // 用指针处理
 #include <stdio.h>
+void my_strcat_ans(char * s, char * t)
+{
+	while('\0' != *s)
+		++s;
+
+	while('\0' != (*s++ = *t++))
+		;
+}
 
 void my_strcat(char * s, char * t)
 {
@@ -19,10 +27,16 @@ int main(void)
 {
 	char s[100] = "abcd";
 	char * t = "efg";
+	char s1[100] = "abcd";
+	char * t1 = "efg";
 
 	printf("s = %s, t = %s\n", s, t);
 	my_strcat(s, t);
 	printf("strcat s = %s\n", s);
+
+	printf("s1 = %s, t1 = %s\n", s1, t1);
+	my_strcat_ans(s1, t1);
+	printf("strcat s1 = %s\n", s1);
 
 	return 0;
 }
